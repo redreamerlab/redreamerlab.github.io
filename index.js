811d9da788cae74b08374047fbfd319af3b569ea
boyu <chuboyu.tw@gmail.com>
2021-09-09 21:24:14 +0800

    contact us

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    document.getElementById("contact").onclick = () => {window.alert(
+        "Coming Soon....\n We are almost ready, you can ping us via \n \n redreamer.nft@gmail.com"
+    )};
+}
