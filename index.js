de256828d81e5a129e5e2fd32d2de25344a20b03
boyu <chuboyu.tw@gmail.com>
2021-09-09 21:37:23 +0800

    add send mail

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.onload = function(){
-    document.getElementById("contact").onclick = () => {window.alert(
-        "Coming Soon....\n We are almost ready, you can ping us via \n \n redreamer.nft@gmail.com"
-    )};
+    
+    document.getElementById("contact").onclick = () => {
+        window.location = "mailto:redreamer.nft@gmail.com";
+        window.alert("Coming Soon....\n We are almost ready, you can ping us via \n \n redreamer.nft@gmail.com")
+    };
 }
