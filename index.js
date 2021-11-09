0aec624cddd35badee3e87c50603b03c982e5304
DBet <chuboyu.tw@gmail.com>
2021-11-09 09:47:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 window.onload = function(){
-    console.log(data)
     document.getElementById("contact").onclick = () => {
-        window.location = "mailto:redreamer.nft@gmail.com";
+        window.location = "mailto:contact@redreamer.io";
         window.alert("Coming Soon....\n We are almost ready, you can ping us via \n \n redreamer.nft@gmail.com")
     };
 }
